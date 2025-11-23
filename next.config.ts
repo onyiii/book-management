a2a5364b1312015e973d53e0f37cbb421b57b1da
Onyinye <onyinyechukwuma14@gmail.com>
2025-11-23 22:44:49 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:3000",
+      ],
+    },
+  },
   /* config options here */
 };
 
